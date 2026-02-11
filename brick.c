a6ca784be220321f1374aeaabd0d49cd94302174
Rajeswari17-28 <lrajeswari663@gmail.com>
2026-02-11 16:04:29 +0530

    Add files via upload

---
+++ brick.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+  int small,big,goal;
+  printf("enter the number of small\n");
+  scanf("%d",&small);
+  printf("enter the number of big\n");
+  scanf("%d",&big);
+  printf("enter the goal in inches");
+  scanf("%d",&goal);
+  int req=goal/5;
+  if (req<=big)
+  {
+      int rem=goal-req*5;
+      if(rem<=small)
+      {
+          printf("true");
+      }
+      else
+      {
+          printf("false");
+      }
+  }
+  else
+  {
+      int rem=goal-big*5;
+      if(rem<=small)
+      {
+          printf("true");
+      }
+      else
+      {
+          printf("false");
+      }
+  }
+  return 0;
+}
